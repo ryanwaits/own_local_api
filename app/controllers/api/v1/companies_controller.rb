6efcf81544d06791ec585b3992fff0c1cdf6d03f
Ryan Waits <ryan.waits@gmail.com>
2016-04-30 18:12:37 -0500

    add pagination to #index

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CompaniesController < Api::V1::BaseController
     before_action :set_company, only: [:show]
     def index
         # all companies
-        respond_with Company.all
+        respond_with Company.paginate(page: params[:page], per_page: 51)
     end
 
     def show
