2425d737e7b12a84aef44115406516eb386d36dd
Ryan Waits <ryan.waits@gmail.com>
2016-04-30 12:42:52 -0500

    use #respond_with to fetch data

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::CompaniesController < Api::V1::BaseController
+    before_action :set_company, only: [:show]
     def index
-
+        # all companies
+        respond_with Company.all
     end
 
     def show
-        
+        # one company
+        respond_with :api, :v1, @company
+    end
+
+    private 
+
+    def set_company
+        @company = Company.find(params[:id])
     end
 end
