91eb7311e1afcbb94a24fa96086b4efca545de2d
Ryan Waits <ryan.waits@gmail.com>
2016-05-02 22:21:36 -0500

    add comments to #company methods

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::CompaniesController < Api::V1::BaseController
     before_action :set_company, only: [:show]
     def index
         # all companies
+        # use the will_paginate gem to display 50 companies per page and order them by {id}
         paginated_companies = Company.paginate(page: params[:page], per_page: 51).order('id ASC')
         Company.add_meta_data(paginated_companies.current_page.to_i, paginated_companies.per_page - 1, Company.all.count)
         respond_with paginated_companies
@@ -12,7 +13,7 @@ class Api::V1::CompaniesController < Api::V1::BaseController
         if Company.valid_id?(params[:id])
             respond_with :api, :v1, @company
         else
-            respond_with Company.error_message_for_id
+            respond_with Company.error_message
         end
     end
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,18 +1,21 @@
 class Company < ActiveRecord::Base
+
+    # add metadata to all #company records
     def self.add_meta_data(page, per_page, total)
         Company.update_all(meta: {page_number: page, per_page: per_page, total_companies: total})
     end
 
+    # check to see if parameter is an Integer
     def self.valid_id?(params)
         params.to_i
         return /\d/.match(params)
     end
 
-    def self.error_message_for_id
+    def self.error_message
         error = [
             {
                 error: {
-                    message: 'Unable to locate specified :id',
+                    message: 'Unable to locate specified {id}',
                     status: 400
                 }
             }
