e8c747a634f31940d18c759f8dc9e81f81801fd7
Ryan Waits <ryan.waits@gmail.com>
2016-05-03 16:42:17 -0500

    write first request test for GET /api/v1/companies.json returning all companies

---
+++ spec/requests/companies_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe '#Companies API' do
+    describe 'GET /api/v1/companies.json' do
+        it 'returns all the companies' do
+            Company.create([
+
+                { 
+                    uuid: '12345', 
+                    name: 'Apple', 
+                    address: '123 Cupertino', 
+                    address2: '', 
+                    city: 'Silicon Valley', 
+                    state: 'CA', 
+                    zip: '98765', 
+                    country: 'US', 
+                    phone: '123-456-7891', 
+                    website: 'http://www.apple.com', 
+                    created_at: Time.now
+                 },
+                 { 
+                    uuid: '67891', 
+                    name: 'Whole Foods', 
+                    address: '525 North Lamar Blvd', 
+                    address2: '', 
+                    city: 'Austin', 
+                    state: 'TX', 
+                    zip: '78703', 
+                    country: 'US', 
+                    phone: '512-476-1206', 
+                    website: 'http://www.wholefoodsmarket.com/stores/lamar?s=LMR', 
+                    created_at: Time.now + 10000
+                 }
+
+                ])
+
+            get '/api/v1/companies.json', {}, { 'Accept' => 'application/json'}
+            expect(response.status).to eq(200)
+
+            body = JSON.parse(response.body)
+            company_names = body.map { |company| company['name'] }
+
+            expect(company_names).to match_array(['Apple', 'Whole Foods'])
+        end
+    end
+end
