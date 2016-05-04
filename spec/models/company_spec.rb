63d9563758ea3cfd90b5a684affba9b12c123b27
Ryan Waits <ryan.waits@gmail.com>
2016-05-03 19:30:56 -0500

    add spec for User, .valid_id?

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Company, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe 'User .valid_id?' do
+    it 'returns nil if {id} is invalid' do
+        valid_num = '5'
+        invalid_num = '-'
+
+        result = Company.valid_id?(invalid_num)
+
+        expect(result).to eq(nil)
+    end
 end
