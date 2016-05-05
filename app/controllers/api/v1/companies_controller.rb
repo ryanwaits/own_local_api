ee49c51963376a27a8d9842aadf91a12a25105f9
Ryan Waits <ryan.waits@gmail.com>
2016-05-05 12:27:41 -0500

    add conditional to controller accepting a per_page parameter

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::CompaniesController < Api::V1::BaseController
     def index
         # all companies
         # use the will_paginate gem to display 50 companies per page and order them by {id}
-        paginated_companies = Company.paginate(page: params[:page], per_page: 50).order('id ASC')
+        if Company.is_valid?(params[:per_page])
+            paginated_companies = Company.paginate(page: params[:page], per_page: params[:per_page]).order('id ASC')
+        else
+            paginated_companies = Company.paginate(page: params[:page], per_page: 50).order('id ASC')
+        end
         Company.add_meta_data(paginated_companies.current_page.to_i, paginated_companies.per_page, Company.all.count)
         
         respond_with paginated_companies
