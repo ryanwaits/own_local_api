cfa9b1492ed2f6fc25afb36c00e5ea72e57875fe
Ryan Waits <ryan.waits@gmail.com>
2016-05-05 14:36:02 -0500

    handle ActiveRecord::RecordNotFound exceptions with JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    respond_with Company.error_message
+  end
 end
