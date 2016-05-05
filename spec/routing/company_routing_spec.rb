8622aeb76fac38700a870264599c31a6b6a653f8
Ryan Waits <ryan.waits@gmail.com>
2016-05-04 21:54:36 -0500

    write routing specs for Api::V1::CompaniesController

---
+++ spec/routing/company_routing_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'support/factory_girl.rb'
+
+describe Api::V1::CompaniesController, type: :routing do
+    it 'routes /api/v1/companies.json to #index' do
+        expect(get('/api/v1/companies.json')).to route_to(controller: 'api/v1/companies', action: 'index', format: 'json')
+    end
+
+    it 'routes /api/v1/companies/1.json to #show' do
+        company = create(:company)
+        expect(get("/api/v1/companies/#{company.id}.json")).to route_to(controller: 'api/v1/companies', action: 'show', id: company.id.to_s, format: 'json')
+    end
+
+end
