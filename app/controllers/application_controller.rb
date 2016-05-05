4c03e2e700a1ccad5607659498023258e56effef
Ryan Waits <ryan.waits@gmail.com>
2016-05-05 15:31:24 -0500

    define #not_found which outputs proper JSON error message for any format passed into routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
 
-  def record_not_found
-    respond_with Company.error_message
+  # render JSON error message
+  def not_found(exception)
+      respond_to do |format|
+        format.any { render json: {error: exception.message, status: 400}, status: :not_found }
+      end
   end
 end
