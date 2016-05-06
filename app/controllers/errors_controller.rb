8e8b667166ab62bfbdc1a0498498625d35b44d56
Ryan Waits <ryan.waits@gmail.com>
2016-05-05 19:39:11 -0500

    change params[:a] to request

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,6 @@
 class ErrorsController < ApplicationController
     def routing
         # call our method defined in ApplicationController, setting a proper error message
-        not_found(ActionController::RoutingError.new("No route matches [#{params[:a].method}] #{params[:a].path}"))
+        not_found(ActionController::RoutingError.new("No route matches [#{request.method}] #{request.path}"))
     end
 end
