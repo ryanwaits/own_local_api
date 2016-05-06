5def61a2125df58056c5beed0a96a8d8473ff10e
Ryan Waits <ryan.waits@gmail.com>
2016-05-05 19:36:32 -0500

    change it block wording

--- spec/routing/api/v1/errors_routing_spec.rb
+++ spec/routing/api/v1/errors_routing_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
-require 'support/factory_girl'
 
 describe ErrorsController, type: :routing do
     describe '#routing' do
-        it 'sets request to params[:a]' do
+        it 'routes /api/v1/anything to #routing action' do
         expect(get('/api/v1/anything')).to route_to(controller: 'errors', action: 'routing', a: 'api/v1/anything')
         end
     end
